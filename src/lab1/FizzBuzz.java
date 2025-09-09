98688111adc450fc11316aff6d96a695913d1efa
pengfan200455 <pengfan200455@gmail.com>
2025-09-09 13:05:17 -0400

    Refactor fizzbuzz, create Main.java

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -9,7 +9,7 @@ class FizzBuzz {
         doFizzBuzz();
     }
 
-    private static void doFizzBuzz() {
+    public static void doFizzBuzz() {
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
