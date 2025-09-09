ed285f6ac771573fb82f55204fe264c8db4d8eaf
pengfan200455 <pengfan200455@gmail.com>
2025-09-09 13:33:48 -0400

    Refactor fizzbuzz, create Multiples.java

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,36 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        giveMultiple();
+    }
+
+    public static void giveMultiple()
+    {
+        for (int i = 1; i < 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
+        }
+    }
+}
